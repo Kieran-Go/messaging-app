a1335a17a2de4ae44c536f6f2cfcabd23ae4f573
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-10-17 08:52:54 +1030

    Implement sub-menu for Friends component
    - Added a sub-menu element for the Friends component
    
    - Added placeholders for unfriend/block functions
    
    - Improvements to layout/UX consistency

--- client/src/components/Friends.jsx
+++ client/src/components/Friends.jsx
@@ -7,7 +7,7 @@ import "../css/Friends.css";
 // Get token from local storage
 const token = localStorage.getItem('token');
 
-// Object to decide which friends list to show
+// Object to decide whether to show accepted friends or friend requests
 const modes = { accepted: 0, requests: 1 };
 
 export default function Friends() {
@@ -15,14 +15,26 @@ export default function Friends() {
     const { user } = useContext(AuthContext);
 
     // ----- STATES -----
+    // Search-bar states
     const [searchFriends, setSearchFriends] = useState("");
     const [searchUsers, setSearchUsers] = useState("");
+
+    // Data states
     const [friendsList, setFriendsList] = useState(null);
     const [filteredFriends, setFilteredFriends] = useState(null);
     const [usersList, setUsersList] = useState(null);
+
+    // State for sub-menu
+    const [subMenu, setSubMenu] = useState({
+        visible: false, x: 0, y: 0, options: []
+    });
+
+    // State to determine whether to show accepted friends or friend requests
+    const [mode, setMode] = useState(modes.accepted);
+
+    // Loading states
     const [loadingFriends, setLoadingFriends] = useState(true);
     const [loadingUsers, setLoadingUsers] = useState(false);
-    const [mode, setMode] = useState(modes.accepted);
 
     // Error message states
     const [friendsListError, setFriendsListError] = useState(null);
@@ -31,14 +43,13 @@ export default function Friends() {
     // ----- EFFECTS -----
     // On first mount
     useEffect(() => {
-        // Fetch user's friends
         const fetchFriends = async () => {
             try{
+                // Fetch user's friends
                 const friends = await getJson("/friendships", null, { token });
+
                 // Sort friends into different categories
-                const accepted = [];
-                const sent = [];
-                const received = [];
+                const accepted = []; const sent = []; const received = [];
                 for(let i = 0; i < friends.length; i++) {
                     const friend = friends[i];
                     if(friend.accepted) accepted.push(friend);
@@ -47,19 +58,22 @@ export default function Friends() {
                         else received.push(friend);
                     }
                 }
+                // Set lists
                 setFriendsList({ accepted, sent, received  });
                 setFilteredFriends(mode === modes.accepted ? accepted : [...sent, ...received]);
             }
             catch (err) {
+                // Set the error
                 setFriendsListError(err.data?.message || err.data?.error ||
                     "Something went wrong while fetching your friends list. Try again later!");
             }
+            // Stop loading after completion
             finally { setLoadingFriends(false) }
         }
         fetchFriends();
     },[]);
 
-    // Update filtered friends whenever searchFriends or friendsList changes
+    // Update filtered friends whenever searchFriends, friendsList or mode changes
     useEffect(() => {
         if (!friendsList) return; // nothing to filter yet
 
@@ -80,7 +94,6 @@ export default function Friends() {
         }
     }, [searchFriends, friendsList, mode]);
 
-
     // ----- FUNCTIONS -----
     // Handle searching for users
    const handleSearchUsers = async () => {
@@ -97,7 +110,10 @@ export default function Friends() {
                 { token }
             );
 
+            // Set users list to fetched data
             setUsersList(data.users);
+
+            // If fetch found no users, display feedback to user with error message
             if(data.users.length <= 0) setUsersListError("No users found.");
         } catch (err) {
             // Show error message
@@ -106,15 +122,46 @@ export default function Friends() {
                 err.data?.error ||
                 "Something went wrong while fetching users. Try again later!"
             );
-        } finally {
+        } 
+        finally {
+            // End loading
             setLoadingUsers(false);
         }
     };
 
+    // Handle sub-menu toggle
+    const handleOptionsClick = (e, options) => {
+        // Click position relative to viewport
+        const { clientX, clientY } = e;
+
+        // Toggle or move submenu
+        setSubMenu({
+            visible: true,
+            x: clientX,
+            y: clientY,
+            options
+        });
+    };
+
+    // Handle unfriending a user
+    const unfriend = async (friendship) => {
+        console.log(`Unfriended ${friendship.friend.username}`);
+        setSubMenu(prev => ({ ...prev, visible: false }));
+    }
+
+    // Handle blocking a user
+    const blockUser = async (target) => {
+        const isFriend = target.friend;
+        const userId = isFriend ? target.friend.id : target.id;
+        console.log(`Blocked user with the ID of ${userId}`);
+        setSubMenu(prev => ({ ...prev, visible: false }));
+    }
+
     // ----- RENDER -----
     return(
+        // Main wrapper of page's content
         <section className="friends-page-wrapper">
-            {/* FRIENDS COLUMN */}
+            {/* ----- FRIENDS COLUMN ----- */}
             <div className="column">
                 {/* Search bar */}
                 <div className="search-bar">
@@ -162,7 +209,23 @@ export default function Friends() {
                                 {filteredFriends.map((f) => {
                                     return (
                                         <div className="list-item" key={f.id}>
-                                            <h1>{f.friend.username}</h1>
+                                            <div className="user-info">
+                                                <h3>{f.friend.username}</h3>
+                                                {f.friend.lastSeen && 
+                                                    <p>{"Last seen: " + f.friend.lastSeen}</p>
+                                                }
+                                            </div>
+
+                                            <div className="user-actions">
+                                                <img src={images.chatFriend}/>
+                                                <img
+                                                    src={images.options}
+                                                    onClick={(e) => handleOptionsClick(e, [
+                                                        { name: "Unfriend", func: () => unfriend(f) },
+                                                        { name: "Block", func: () => blockUser(f) }
+                                                    ])}
+                                                />
+                                            </div>
                                         </div>
                                     )
                                 })}
@@ -175,8 +238,21 @@ export default function Friends() {
                         {filteredFriends
                             ?.filter(f => !f.isRequester)
                             .map(f => (
-                                <div className="list-item" key={f.id}>
-                                    <h3>{f.friend.username}</h3>
+                                 <div className="list-item" key={f.id}>
+                                    <div className="user-info">
+                                        <h3>{f.friend.username}</h3>
+                                    </div>
+
+                                    <div className="user-actions">
+                                        <button className="add-friend-btn">Accept</button>
+                                        <button className="decline-friend-btn">Decline</button>
+                                        <img
+                                            src={images.options}
+                                            onClick={(e) => handleOptionsClick(e, [
+                                                { name: "Block", func: () => blockUser(f) }
+                                            ])}
+                                        />
+                                    </div>
                                 </div>
                         ))}
 
@@ -185,15 +261,27 @@ export default function Friends() {
                         {filteredFriends
                             ?.filter(f => f.isRequester)
                             .map(f => (
-                                <div className="list-item" key={f.id}>
+                            <div className="list-item" key={f.id}>
+                                <div className="user-info">
                                     <h3>{f.friend.username}</h3>
                                 </div>
+
+                                <div className="user-actions">
+                                    <button className="decline-friend-btn">Delete</button>
+                                    <img
+                                        src={images.options}
+                                        onClick={(e) => handleOptionsClick(e, [
+                                            { name: "Block", func: () => blockUser(f) }
+                                        ])}
+                                    />
+                                </div>
+                            </div>
                         ))}
                     </div>
                 )}
             </div>
 
-            {/* USERS COLUMN */}
+            {/* ----- USERS COLUMN ----- */}
             <div className="column">
                 {/* Search bar */}
                 <div className="search-bar">
@@ -226,7 +314,23 @@ export default function Friends() {
                         <div className="list">
                             {usersList.map((u) => (
                                 <div className="list-item" key={u.id}>
-                                    <h3>{u.username}</h3>
+                                    <div className="user-info">
+                                        <h3>{u.username}</h3>
+                                        {u.lastSeen && 
+                                            <p>{"Last seen: " + u.lastSeen}</p>
+                                        }
+                                    </div>
+
+                                    <div className="user-actions">
+                                        <img src={images.chatFriend}/>
+                                        {!u.isFriend && <button className="add-friend-btn">Add Friend</button> }
+                                        <img
+                                            src={images.options}
+                                            onClick={(e) => handleOptionsClick(e, [
+                                                { name: "Block", func: () => blockUser(u) }
+                                            ])}
+                                        />
+                                    </div>
                                 </div>
                             ))}
                         </div>
@@ -237,6 +341,26 @@ export default function Friends() {
                     <p>Search for users to add as friends</p>
                 )}
             </div>
+
+            {/* Backdrop to block interaction with page elements while sub-menu is open */}
+            {subMenu.visible && (
+                <div
+                    className="submenu-backdrop"
+                    onClick={() => setSubMenu(prev => ({ ...prev, visible: false }))}
+                ></div>
+            )}
+
+            {/* Sub-menu */}
+            {subMenu.visible && (
+                <div
+                    className="sub-menu"
+                    style={{ top: subMenu.y, left: subMenu.x - 80 }}
+                >
+                    {subMenu.options.map((o) => {
+                        return(<p key={o.name} onClick={() => o.func()}>{o.name}</p>);
+                    })}
+                </div>
+            )}
         </section>
     );
 }
--- client/src/utility/images.js
+++ client/src/utility/images.js
@@ -1,21 +1,25 @@
 import account from '../assets/img/account.svg';
 import chat from '../assets/img/chat.svg';
+import chatFriend from '../assets/img/chat.svg'
 import close from '../assets/img/close.svg';
 import heart from '../assets/img/heart.svg';
 import logo from '../assets/img/logo.svg';
 import logout from '../assets/img/logout.svg';
 import menu from '../assets/img/menu.svg';
+import options from '../assets/img/options.svg';
 import profile from '../assets/img/profile.svg';
 import search from "../assets/img/search.svg";
 
 export default {
     account,
     chat,
+    chatFriend,
     close,
     heart,
     logo,
     logout,
     menu,
+    options,
     profile,
     search,
 }
